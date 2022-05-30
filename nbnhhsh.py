6fc2ea9d0fc614dd6bb1fef95972ae851c31b3e1
user <user@DELL-7420>
2022-05-31 02:27:56 +0800

    change to aiohttp

--- nbnhhsh.py
+++ nbnhhsh.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import re
-import json
+import aiohttp
 
 from hoshino import Service
 from hoshino.typing import CQEvent
 from hoshino.util import escape
-from hoshino.aiorequests import post
 
 sv = Service('能不能好好说话？', help_='''
 [.guess 缩写] 查询
@@ -15,23 +17,17 @@ async def query(text: str) -> str:
         return '太、太长了8…'
     if not re.match(r'^[a-zA-Z0-9]+$', text):
         return '只能包含字母'
-    rsp = await post(
-        'https://lab.magiconch.com/api/nbnhhsh/guess',
-        headers={'content-type': 'application/json'},
-        data=json.dumps({'text': text}),
-    )
-    rsp = await rsp.json()
+    async with aiohttp.ClientSession(raise_for_status=True) as session:
+        async with session.post('https://lab.magiconch.com/api/nbnhhsh/guess', data={'text': text}) as resp:
+            ret = await resp.json()
     result = []
-    for item in rsp:
+    for item in ret:
         prefix = '[%s]: ' % item.get('name')
         trans = item.get('trans')
         if trans == None:
-            inputting = item.get('inputting')
-            if inputting == None:
-                inputting = []
-            inputting = ', '.join(inputting)
+            inputting = ', '.join(item.get('inputting', []))
             if inputting == '':
-                ans = '最佳答案：我不知道'
+                ans = '优质解答：我不知道'
             else:
                 ans = '有可能是：' + inputting
         else:
@@ -43,10 +39,12 @@ async def query(text: str) -> str:
 async def guess(bot, ev: CQEvent):
     s = escape(ev.message.extract_plain_text().strip())
     msg = await query(s)
-    await bot.send(ev, msg, at_sender=True)
+    if msg:
+        await bot.send(ev, msg, at_sender=True)
 
 @sv.on_rex(re.compile(r'^\s*(?P<text>[a-zA-Z0-9]+)是(什么|甚么|啥|？|\?)'))
 async def guess_re(bot, ev: CQEvent):
     match = ev['match']
     msg = await query(match.group('text'))
-    await bot.send(ev, msg, at_sender=True)
+    if msg:
+        await bot.send(ev, msg, at_sender=True)
